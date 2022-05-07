dc4ccd3fb9fddf8d215232030a0e4b9892e0ae84
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-07 21:47:25 +0530

    Added to PowerOf2

---
+++ Day05/PowerOfTwo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    internal class PowerOfTwo
+    {
+        public void Power()
+        {
+            Console.Write("Enter the power of 2 i.e., value of N (Keep  0 <= N < 31 since 2^31 overflows an int): ");
+            try
+            {
+                uint N = Convert.ToUInt32(Console.ReadLine());
+                if (N >= 0 && N < 31)
+                {
+
+                    for (int i = 0; i <= N; i++)
+                    {
+                        Console.WriteLine("The value 2^" + i + " is : " + Math.Pow(2, i));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entered value is too large to handle");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Entered value is negative");
+            }
+
+
+        }
+    }
+}
--- Day05/Program.cs
+++ Day05/Program.cs
@@ -9,6 +9,7 @@ namespace Day05
             Console.WriteLine("Enter the choice");
             Console.WriteLine("1. Flip Coin and print percentage of Heads and Tails");
             Console.WriteLine("2. Leap Year");
+            Console.WriteLine("3. Power of 2");
 
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -20,6 +21,10 @@ namespace Day05
                 case 2:
                     LeapYear ly = new LeapYear();
                     ly.Year();
+                    break;
+                case 3:
+                    PowerOfTwo pw = new PowerOfTwo();
+                    pw.Power();
                     break;
                 default:
                     Console.WriteLine("Please enter the correct choice");
