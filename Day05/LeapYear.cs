6b99b607d21ebe4fb4a3dea8cfad6566b4c0e79c
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-07 21:42:27 +0530

    Added to LeapYear

---
+++ Day05/LeapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    internal class LeapYear
+    {
+        public void Year()
+        {
+            Console.Write("Enter a 4 digit year : ");
+            uint year = Convert.ToUInt32(Console.ReadLine());
+
+            //Checking if the entered value is 4 digit or not
+            if (year > 999 && year <= 9999)
+            {
+                //Leap Year condition
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                {
+                    Console.WriteLine("Year is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine("Year is not Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered year is not 4 digit");
+            }
+
+        }
+    }
+}
--- Day05/Program.cs
+++ Day05/Program.cs
@@ -8,6 +8,7 @@ namespace Day05
         {
             Console.WriteLine("Enter the choice");
             Console.WriteLine("1. Flip Coin and print percentage of Heads and Tails");
+            Console.WriteLine("2. Leap Year");
 
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -15,6 +16,10 @@ namespace Day05
                 case 1:
                     FlipCoin fp = new FlipCoin();
                     fp.Coin();
+                    break;
+                case 2:
+                    LeapYear ly = new LeapYear();
+                    ly.Year();
                     break;
                 default:
                     Console.WriteLine("Please enter the correct choice");
